685866cde2174e5c580c7f63d303df5ea8080ac4
Matvey <mat20061119@gmail.com>
2021-10-20 21:46:42 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace ProjectParserWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridView1.Location = new System.Drawing.Point(165, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(920, 542);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Id";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Название";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Место";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Цена";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Описание";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Опубликовано";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Ссылка на обьявление";
+            this.Column8.Name = "Column8";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 473);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество страниц";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 604);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open in Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(179, 621);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(326, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 595);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Идет загрузка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 709);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using Newtonsoft.Json;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Threading;
+
+namespace ProjectParserWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.BackColor = Color.Aqua;
+            dataGridView1.Columns[4].Width += 50;
+            dataGridView1.Columns[6].Width = 250;
+            progressBar1.Visible = false;
+            label2.Visible = false;
+        }
+        public class AnnouncementInfo
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Place { get; set; }
+            public string Price { get; set; }
+            public string Description { get; set; }
+            public string TimePublished { get; set; }
+            public string Url { get; set; }
+            public AnnouncementInfo()
+            {
+
+            }
+        }
+        List<AnnouncementInfo> ans = new List<AnnouncementInfo>();
+        private void AddToTable(AnnouncementInfo ai)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.CreateCells(dataGridView1);
+            row.Cells[0].Value = ai.Id;
+            row.Cells[1].Value = ai.Name;
+            row.Cells[2].Value = ai.Place;
+            row.Cells[3].Value = ai.Price;
+            row.Cells[4].Value = ai.Description;
+            row.Cells[5].Value = ai.TimePublished;
+            row.Cells[6].Value = ai.Url;
+            dataGridView1.Rows.Add(row);
+
+        }
+        void Write(string name)
+        {
+            List<string> names = new List<string>() { "Id", "Название", "Место", "Цена", "Описание", "Опубликовано", "Ссылка на объявление" };
+            var memoryStream = new MemoryStream();
+            DataTable table = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(ans), (typeof(DataTable)));
+            progressBar1.Value = 0;
+            using (var fs = new FileStream(name + ".xlsx", FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Sheesh");
+                List<String> columns = new List<string>();
+                IRow row = excelSheet.CreateRow(0);
+                for (int i = 0; i < names.Count; i++)
+                {
+                    columns.Add(table.Columns[i].ColumnName);
+                    row.CreateCell(i).SetCellValue(names[i]);
+                }
+                int rowIndex = 1;
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    int cellIndex = 0;
+                    row = excelSheet.CreateRow(rowIndex);
+                    foreach (String col in columns)
+                    {
+                        row.CreateCell(cellIndex).SetCellValue(dsrow[col].ToString());
+                        cellIndex++;
+                    }
+                    rowIndex++;
+                    progressBar1.Value++;
+                }
+                workbook.Write(fs);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            label2.Visible = false;
+            dataGridView1.Rows.Clear();
+            ans.Clear();
+            button1.Enabled = false;
+            Task.Run(new Action(() =>
+            {
+                for (int i = 0; i < numericUpDown1.Value; i++)
+                {
+                    var url = @"https://www.kijiji.ca/b-canada/iphone/page-" + (i + 1).ToString() + @"/k0l0?rb=true&dc=true";
+                    var client = new HtmlWeb();
+                    var html = client.Load(url);
+                    var nodes = html.DocumentNode.QuerySelectorAll("div[data-listing-id]").ToList();
+                    foreach (var item in nodes)
+                    {
+                        AnnouncementInfo ai = new AnnouncementInfo();
+                        ai.Id = Regex.Replace(item.GetAttributeValue("data-listing-id", "").Trim(), @"\s{2}", "");
+                        ai.Name = Regex.Replace(item.QuerySelector("div.title").InnerText.Trim(), @"\s{2}", "");
+                        ai.Place = Regex.Replace(item.QuerySelector("div.location").InnerText.Trim(), @"\s{2}", "");
+                        ai.Price = Regex.Replace(item.QuerySelector("div.price").InnerText.Trim(), @"\s{2}", "");
+                        ai.Description = Regex.Replace(item.QuerySelector("div.description").InnerText.Trim(), @"\s{2}", "");
+                        var d = item.QuerySelector("span.date-posted");
+                        if (d != null)
+                        {
+                            ai.TimePublished = Regex.Replace(d.InnerText.Trim(), @"\s{2}", "");
+                        }
+                        ai.Url = Regex.Replace(@"https://www.kijiji.ca" + item.GetAttributeValue("data-vip-url", ""), @"\s{2}", "");
+                        ans.Add(ai);
+                        AddToTable(ai);
+                    }
+                }
+            }));
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ans.Count==0)
+            {
+                MessageBox.Show("Вы не заполнили таблицу!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = ans.Count;
+                progressBar1.Visible = true;
+                label2.Visible = true;
+                Write("Announcments");
+                Task.Run(new Action(() =>
+                {
+                    Thread.Sleep(2000);
+                    System.Diagnostics.Process.Start("Announcments.xlsx");
+                }));
+                
+            }
+        }
+    }
+}
